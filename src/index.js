3b0fb0dda1c459440973f65ebf6df3e99b8c25ab
OksanaGukova <152022817+OksanaGukova@users.noreply.github.com>
2026-06-30 16:53:53 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.use(cors());
